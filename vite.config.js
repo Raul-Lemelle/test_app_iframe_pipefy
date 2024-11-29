95fe0ae6ec451cfdd606d058de9c5ed1c755fcd7
${GITHUB_USER} <${GITHUB_MAIL}>
2024-11-29 20:30:53 -0300

    sos agora vai

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: "build"
   },
   server: {
     host: "0.0.0.0",
     port: 3000,
     strictPort: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
-    },
     hmr: {
-      clientPort: 443, // Run the websocket server on the SSL port
-    },
-  },
+      clientPort: 443 // Run the websocket server on the SSL port
+    }
+  }
 });
